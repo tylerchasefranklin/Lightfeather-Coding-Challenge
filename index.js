370766cfdde44c594b2bd9ba04df3a868be135aa
Chase Franklin <tylerchasefranklin@gmail.com>
2021-09-14 23:23:05 -0400

    Making sure commits are working correctly

--- index.js
+++ index.js
@@ -16,18 +16,11 @@ $(document).ready(function(){
                $.each(response.results, function(supervisors, supervisor){
                  var supervisorFirstName = supervisor.name.first;
                  var supervisorLastName = supervisor.name.last;
-                 console.log(supervisorLastName);
-                 // console.log(supervisorFirstName);
                  var toAppend = '';
                  toAppend += '<option>'+supervisorFirstName + ' ' + supervisorLastName+'</option>';
                  $('#supervisor').append(toAppend);
                });
-               // var toAppend = '';
-               // $.each(response.results, function(i,o){
-               //   toAppend += '<option>'+o.id+'</option>';
-               // });
 
-               // $('#supervisor').append(toAppend);
            },
            error: function (xhr, status) {
                alert("error");
